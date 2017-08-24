fd1d91546ed4d3b03d7d40916541fc78f14ebfb7
Lucas Chescheir <lucaschescheir@gmail.com>
2017-08-24 11:36:13 -0400

    seond commit

--- server.js
+++ server.js
@@ -2,4 +2,6 @@ const hapi = require('hapi');
 const mongoose = require('mongoose');
 const server = new hapi.Server();
 
+mongoose.Promise = require('bluebird');
+// Replace "test" with your database name.
 mongoose.connect('mongodb://localhost:27017/test');
